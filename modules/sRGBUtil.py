d8a8df50f574885e7102a2732d46ed252ce7a9d9
Moltenhead <ectopneo@gmail.com>
2019-02-16 17:57:52 +0100

    transfered gamma functions to Vectors to rediuce the import amount and avoid cycling calls

--- modules/sRGBUtil.py
+++ modules/sRGBUtil.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-
-import sys
-sys.path.append(".\\")
-import vectorizedMatrix as vmat
+import  numpy            as np
+# Home brewed
+from sys import path
+path.append('.')
+from    .vectorizedMatrix  import Vec3
 
 #**********
 class SRGB:
@@ -14,7 +14,7 @@ class SRGB:
     self.G = g
     self.B = b
 
-    self.__sRGBVec = vmat.Vec3(r,g,b)
+    self.__sRGBVec = Vec3(r,g,b)
   
   def get_sRGBVec(self):
     return self.__sRGBVec
@@ -33,15 +33,3 @@ def RGBToSRGB(rgb):
   sG = int(baseG)/(int(baseR) + int(baseG) + int(baseB))                       # normalized green
   sB = int(baseB)/(int(baseR) + int(baseG) + int(baseB))                       # normalized blue
   return SRGB(I, sR, sG, sB)
-
-#**************************************************************
-def gammaExpand(nonlinear):
-# Convert an sRGB color channel to a linear sRGB color channel.
-#**************************************************************
-  return (nonlinear / 12.92) if (nonlinear <= 0.04045) else (np.float_power((nonlinear+0.055)/1.055, 2.4))
-
-#*************************************************************
-def gammaCompress(linear):
-# Convert a linear sRGB color channel to a sRGB color channel.
-#*************************************************************
-  return (12.92*linear) if (linear <= 0.0031308) else (1.055 * np.float_power(linear, 1.0/2.4) - 0.055)

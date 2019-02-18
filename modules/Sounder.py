b5f96953aa8501f28e8564cfb0fc4147a1bde3de
Moltenhead <ectopneo@gmail.com>
2019-02-18 20:15:58 +0100

    404 hair not found : please replant before tearing off ; searching how to match XYZ pos within limited data

--- modules/Sounder.py
+++ modules/Sounder.py
@@ -4,11 +4,13 @@ from __future__ import division
 __package__  = './'
 # Ext
 import  csv
-from    pyaudio        import PyAudio
+import  numpy               as     np
+from    pyaudio             import PyAudio
+from    scipy.interpolate   import griddata 
 # Local
 import  math
 # Home brewed
-from    os            import path, pardir
+from    os                  import path, pardir
 
 # ------ CONSTANTS ------ #
 ROOT            = path.dirname(path.abspath(path.join(__file__, pardir)))
@@ -22,7 +24,7 @@ class Sounder:
   # ----------------------
   def __init__(self):
   # ----------------------
-    self.cursor = 0
+    self.cursor = -1
 
     self.__wavelengthTable = []
     with open(CONVERTER_PATH, 'r') as csvfile:
@@ -30,7 +32,19 @@ class Sounder:
       for row in dictReader:
         self.__wavelengthTable.append(row)
       csvfile.close()
-  
+    
+    self.table = {
+      'X': [],
+      'Y': [],
+      'Z': [],
+      'WL': []
+    }
+    for row in self.__wavelengthTable:
+      self.table['X'].append(float(row['X']))
+      self.table['Y'].append(float(row['Y']))
+      self.table['Z'].append(float(row['Z']))
+      self.table['WL'].append(float(row['WL']))
+
   # ------------------------------------
   def get_tableAt(self, rowIdx,  label):
   # ------------------------------------
@@ -51,7 +65,19 @@ class Sounder:
   # --------------------
   def cursorReset(self):
   # --------------------
-    self.cursor = 0
+    self.cursor = -1
+  
+  def findNearestAt(self, matchValue, label):
+    array = np.asarray(self.table[label])
+    idx = (np.abs(array - matchValue)).argmin()
+    return idx
+  
+  def interpolate(self, matchValue):
+    points = np.array([self.x, self.y, self.z])
+    grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
+    grid = griddata(points, matchValue, (grid_x, grid_y), method='nearest')
+    print(grid)
+    return grid
 
   # -----------------------------------------
   def cursorMatchAt(self, matchValue, label):
@@ -64,25 +90,51 @@ class Sounder:
     return self.get_cursorAt(label)
   
   # -------------------------------------
+  def aproxMatch(self, XYZArray):
+  # -------------------------------------
+    x = float(round(XYZArray[0], 4))
+    y = float(round(XYZArray[1], 4))
+    z = float(round(XYZArray[2], 4))
+    # print("[{}:{}:{}]".format(x,y,z))
+    xfound = self.findNearestAt(x, 'X')
+    yfound = self.findNearestAt(y, 'Y')
+    zfound = self.findNearestAt(z, 'Z')
+    # print("[{}:{}:{}]".format(xfound,yfound,zfound))
+    
+    mx = max([xfound, yfound, zfound])
+    mn = min([xfound, yfound, zfound])
+    diff = mx - mn
+
+    if diff > 5:
+      return False
+   
+    print("[{}:{}:{}]".format(xfound,yfound,zfound))
+    return (xfound + yfound + zfound)/3
+
+  # -------------------------------------
   def cursorIterateMatch(self, XYZArray):
   # -------------------------------------
-    x = round(XYZArray[0]*10, 4)
-    y = round(XYZArray[1]*10, 4)
-    z = round(XYZArray[2]*10, 4)
-    print("[{}:{}:{}]".format(x,y,z))
+    x = float(round(XYZArray[0]*10, 4))
+    y = float(round(XYZArray[1]*10, 4))
+    z = float(round(XYZArray[2]*10, 4))
+    # print("[{}:{}:{}]".format(x,y,z))
+    # self.findNearestAt(XYZArray)
     found = False
     while found == False:
-      if self.cursor < self.get_tableLength():
+      if self.cursor >= self.get_tableLength():
         break
       found = self.cursorMatchAt(x, 'X')
       if found != False:
+        print("x:{}".format(x))
         found = self.cursorMatchAt(y, 'Y')
         if found != False:
+          print("y:{}".format(y))
           found = self.cursorMatchAt(z, 'Z')
           if found != False:
+            print("z:{}".format(z))
             self.cursorReset
             return found
-      
+      # print(found)
       found = False
       self.cursorReset()
       return found
@@ -90,12 +142,14 @@ class Sounder:
   # --------------------------------
   def findWaveAtXYZ(self, XYZArray):
   # --------------------------------
-    found = self.cursorIterateMatch(XYZArray)
+    # found = self.cursorIterateMatch(XYZArray)
+    found = self.aproxMatch(XYZArray)
+    # print(found)
     if found is False:
-      print("Ended with no match for {}:{}:{}".format(
-        XYZArray[0],
-        XYZArray[1],
-        XYZArray[2]))
+      # print("Ended with no match for {}:{}:{}".format(
+      #   XYZArray[0],
+      #   XYZArray[1],
+      #   XYZArray[2]))
       return found
     
     if isinstance(found, list):
@@ -131,8 +185,9 @@ class Sounder:
 
   def requestXYZPlay(self, XYZArray, seconds):
     wavelength = self.findWaveAtXYZ(XYZArray)
-    print(wavelength)
+    # print(wavelength)
     if wavelength is False:
+      # print("    No match.")
       return
     
     toTHz = convertWlToTHz(wavelength)
@@ -145,7 +200,3 @@ def convertWlToTHz(wavelength):
   if isinstance(wavelength, float):
     return THZ_TO_NM / wavelength
   return False
-
-    
-
-

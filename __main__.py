2e74a816938d1a90ec978360dd01b31101a6ed29
Moltenhead <ectopneo@gmail.com>
2019-02-20 01:01:34 +0100

    removed unused code and imports

--- __main__.py
+++ __main__.py
@@ -1,36 +1,13 @@
 # -*- coding: utf-8 -*-
-if __name__ == '__main__':                                                        # ensure following is only called within main.py
+if __name__ == '__main__':                                                        # ensure following code is only called within main.py
   '''
     Created by Charlie GARDAI alias Moltenhead - 2019
     _________________________________________________
   '''
   # ------ IMPORTS ------ #
-  # Ext
-  # import numpy              as np
-  # Local
-  # import re
-  # import glob
-  # import time
-  # import multiprocessing
-  # import os.path            as path
-  # Home brewed
-  # import  modules.RGBUtil  as RGBUtil
-  # from    modules.vectorizedMatrix        import Vec2
   from    modules.Imager                  import Imager
   from    modules.Sounder                 import Sounder
-  # from    modules.ColorSpace              import ColorSpace
-  # from    modules.processUtil             import splitPixarray, pixelProcess
-  ''' Will be needed once MATH
-  MAX_PROCESS_NB = 1
 
-  # ------ COLORSPACE DEFINITION ------ #
-  red_xy     = Vec2(0.64, 0.32)                                                   # Red           Coordinates vectors
-  green_xy   = Vec2(0.30, 0.60)                                                   # Green         Coordinates vectors
-  blue_xy    = Vec2(0.15, 0.06)                                                   # Blue          Coordinates vectors
-  white_xy   = Vec2(0.3127, 0.3290)                                               # White         Coordinates vectors
-
-  sRGBSpace  = ColorSpace(red_xy, green_xy, blue_xy, white_xy)                    # sRGB          ColorSpace
-  '''
   # ------ IMG HANDLER ------ #
   imager = Imager()                                                               # create image handler
   imager.requestImgCursor()                                                       # request user input
@@ -39,22 +16,5 @@ if __name__ == '__main__':                                                      
 
   # ------ PIXEL BY PIXEL PROCESS ------ #
   sounder = Sounder()                                                             # create sound handler
-  sounder.pixarrayToNoise(img)#, sRGBSpace)                                       # register noise from a pixarray
+  sounder.pixarrayToNoise(img)                                                    # register noise from a pixarray
   sounder.playNoise()                                                             # play
-
-  ''' MULTIPROC TEST, NO USE FOR THIS VERSION
-  # pixelCount       = []
-  # filledPixelCount = []
-
-  # splitted = splitPixarray(MAX_PROCESS_NB, img)
-  # processes = []  
-  
-  # for i in range(len(splitted)):
-  #   p = multiprocessing.Process(
-  #     name="pixelProcess{}".format(i),
-  #     target=pixelProcess(pixelCount, filledPixelCount, splitted[i], sRGBSpace, sounder))
-  #   processes.append(p)
-  #   p.start()
-  # for p in processes:
-  #   p.join()
-  '''

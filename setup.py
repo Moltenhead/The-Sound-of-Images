88b33b946e79ba6911e9ca4fcddbc777238d01ea
Moltenhead <ectopneo@gmail.com>
2019-02-20 03:06:16 +0100

    made a true setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
-install_requires=[
-  'matplotlib>=1.4',
-  'numpy>=1.16.1',
-  'pyaudio>=0.2.11'
-]
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+
+setup(
+    name='soundofimages',
+    version=1.0,
+    packages=find_packages(),
+    author="Charlie GARDAI / Moltenhead",
+    author_email="ectopneo@gmail.com",
+    description="Produce sounds, based on images",
+    long_description=open('README.md').read(),
+    install_requires=[
+      'matplotlib>=1.4',
+      'numpy>=1.16.1',
+      'pyaudio>=0.2.11'
+    ],
+    url='https://github.com/Moltenhead/The-Sound-of-Images',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 2 - Expanding",
+        "License :: GPL-3.0",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3+",
+        "Topic :: Image perception and sound",
+    ]
+)

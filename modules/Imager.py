ad6820bdfdb835dd7fb2125debcc331909103708
Moltenhead <ectopneo@gmail.com>
2019-02-20 01:02:43 +0100

    beautifying and comments

--- modules/Imager.py
+++ modules/Imager.py
@@ -13,11 +13,15 @@ from    fractions import Fraction
 from    os        import path, pardir
 
 ROOT              = path.dirname(path.abspath(path.join(__file__, pardir)))
-TRANSFORMED_PATH  = ROOT + "\\.transformed\\"
-MAX_PIXELS        = int(10**2 * 1.5)
+TRANSFORMED_PATH  = ROOT + "\\.transformed\\"                                      # where are stored transformed imgs
+MAX_PIXELS        = int(10**2 * 1.5)                                               # max pixel treshold
 
+#************
 class Imager:
+#************
+  # ------------------------------------------------------------------
   def __init__(self, ext = ['.png', '.jpg'], img_paths= ['.\\img\\']):
+  # ------------------------------------------------------------------
     self.img_ext = ext
     self.img_paths = img_paths
 
@@ -28,13 +32,17 @@ class Imager:
 
     self.storeImgs()
 
+  # --------------------------------
   def appendImg(self, img_fullPath):
+  # --------------------------------
     self.img_list.append({
       'name': re.split(r'\\', img_fullPath)[-1],
       'fullPath': img_fullPath,
       'resized': None})
-
+  
+  # ------------------
   def storeImgs(self):
+  # ------------------
     imgCount = 0
     for path in self.img_paths:
       for ext in self.img_ext:
@@ -42,24 +50,34 @@ class Imager:
           self.appendImg(img_fullPath)
           imgCount += 1
   
+  # -----------------------------------------
   def get_img(self, img_idx, part = 'whole'):
+  # -----------------------------------------
     if part is 'whole':
       return self.img_list[img_idx]
     else:
       return self.img_list[img_idx][part]
   
+  # ------------------------------------
   def imgAtCursor(self, part = 'whole'):
+  # ------------------------------------
     return self.get_img(self.cursor, part)
   
+  # -----------------
   def getImgNb(self):
+  # -----------------
     return len(self.img_list)
   
+  # -----------------------
   def showStoredImgs(self):
+  # -----------------------
     print("\n  {} images found in specified directories:".format(self.getImgNb()))
     for idx, img in enumerate(self.img_list):
       print("    [{}]: {}".format(idx, img))
 
+  # ----------------------------
   def error(self, errorIdx = 0):
+  # ----------------------------
     string = ""
     if errorIdx is 0:
       string = "\n\n  Wrong selection."
@@ -69,7 +87,9 @@ class Imager:
     
     return False
 
+  # -------------------------
   def requestImgCursor(self):
+  # -------------------------
     if self.getImgNb() <= 0:
       sys.exit("\n  No images to choose from.")
     
@@ -86,7 +106,9 @@ class Imager:
       else:
         userInput = self.error()
 
+  # ---------------------------------------
   def shrinkImg(self, img_idx, maxPixelNb):
+  # ---------------------------------------
     targetImg = self.get_img(img_idx)
     resized = None
     with Image.open(targetImg['fullPath']) as img:
@@ -105,7 +127,11 @@ class Imager:
     self.img_list[self.cursor]['resized'] = TRANSFORMED_PATH + "resized_" + targetImg['name']
     return self.imgAtCursor()
   
+  # ----------------------------------
   def verifySize(self):
+  # verify if img size is not too long
+  # shrink if user asks
+  # ----------------------------------
     target = self.imgAtCursor()
     img = Image.open(target['fullPath'])
     pixelsNb = img.size[0] * img.size[1]
@@ -122,13 +148,16 @@ class Imager:
         else:
           userInput = self.error()
   
+  # ------------------------------
   def getCursorToPixarray(self):
+  # returns img at cursor position
+  # ------------------------------
     resized = self.imgAtCursor('resized')
     targetLabel = 'fullPath'
-    if resized is not None:
+    if resized is not None:                                         # select resized img if resized img is registered
       targetLabel = 'resized'
     img = mpimg.imread(self.imgAtCursor(targetLabel))
     
-    if img.dtype == np.uint8:                                                        # if result isn't integer array
+    if img.dtype == np.uint8:                                       # if result isn't integer array
       img   = (img / 255).astype(np.float32)
     return img
